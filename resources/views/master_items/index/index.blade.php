17d9f217bea51520fd13c263d143a3f555b180c3
ellikbarek <kbarekelisa@gmail.com>
2026-09-11 17:13:45 +0900

    update master items

--- resources/views/master_items/index/index.blade.php
+++ resources/views/master_items/index/index.blade.php
@@ -8,7 +8,7 @@
                 <a href="{{url('master-items/form/new')}}" class="btn btn-secondary">+ Master Items Baru</a>
             </div>
             <div class="card">
-                <div class="card-header">git Daftar Master Items Test Update</div>
+                <div class="card-header">git Daftar Master Items</div>
 
                 <div class="card-body">
                     @include('master_items.index.filter')
